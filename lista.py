41ce3d2168a616faef07137b5fae6c0fc7b9bedf
Biancabonattog <biancabonatto@outlook.com>
2024-08-28 17:47:02 -0300

    Update and rename README.md to lista.py

---
+++ lista.py
@@ -0,0 +1,50 @@
+# lista-de-compras
+
+#1.criar def para lista de produtos
+#2. criar def para menu
+   #a. Criar saudacao + isso é uma lista de compras
+   #b. lista de produtos
+   #b. Adicionar produto
+   #c. Remover produto
+   #d. Pesquisar produto
+   #e. Sair do programa
+#3. criar condicao para entradas invalidas
+#4. criar condicao para encerramento
+#5. criar condicao para Adicionar produto
+    #a. Nome
+    #b. Unidade de medida float
+    #c. Quantidade int
+    #c. Descricao
+#6. criar ID - 0 + 1 e o sitema vai adicionando
+#7. criar condicao para remover produto 
+#8. criar condicao para pesquisar produtos 
+
+def lista_de_produtos = [produtos]
+
+def menu ():
+    while True: # loop sempre vai executar
+        print ("Olá, essa é a sua lista de compras:") 
+        print (lista_de_produtos)
+        print ("1. Adicionar produto") #opções de menu
+        print ("2. Remover produto")
+        print ("3. Pesquisar produto")
+        operacao = input ("Selecione uma opção ou 's' para sair: ")
+
+        if operacao == 's' or operacao == 'S':
+            print("Voce saiu do aplicacao lista de compras. Até já!")
+            break #termina a execução do loop
+
+        if operacao is not in = [1,2,3]:
+            print("Você inseriu uma opcao invalida! Tente novamente.")
+            continue
+
+        if operacao == '1':
+        def adicionar ():
+        nome = input ("Qual o nome do produto?")
+        print("Quilograma")
+        print ("Grama")
+        
+        unidade = input ("'Escolha uma unidade de medida?")
+        
+
+    
